4df238785b60ac065accc9d183bf5f1a28f0ffa1
ikda <akusiron@gmail.com>
2019-04-08 16:15:11 +0900

    route signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-
+import { HashRouter as Router, Route} from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
+import signup from "./signup";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>
+    return <ApolloProvider client={client}>
+      <Router>
+        <React.Fragment>
+          <Route exact={true} path="/signup" component={signup} />
+        </React.Fragment>
+      </Router>
+    </ApolloProvider>
   }
 }
 
---
+++ src/signup.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const signup = () => "signup";
+
+export default signup;
