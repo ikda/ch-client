acaec050d4dc959d346d5e199b9ad0a1ae20c63d
wwlee0405 <wwlee0405@gmail.com>
2019-08-20 12:41:24 +0900

    layout0820

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,55 +1,66 @@
-import React, {Component} from 'react';
+import React from "react";
 import styled from "styled-components";
-import HomeLayout from "../Components/Layout/HomeLayout";
-import ClubhouseHome from "../Components/Layout/ClubhouseHome";
-import Profile from "../Components/Layout/Profile";
-import Photo from "../Components/Layout/Photo";
-import MySchedule from "../Components/Layout/MySchedule";
-import MyRank from "../Components/Layout/MyRank";
 
-const ChTittle = styled.div`
-  grid-column: 1;
-  grid-row: 1;
+const HomeLayout = styled.div`
+	display: grid;
+	grid-auto-rows: 200px;
+	grid-template-rows: repeat(auto-fit, 1fr);
+	background-color: grey;
 `;
 
-const Spacer = styled.div`
-  grid-column: 2;
-  grid-row: 1;
+const Header = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
 `;
 
-const ChTittleItems = styled.div`
-  grid-column: 3;
-  grid-row: 1;
+const ProfileLogo = styled.div`
+	display: flex;
+	justify-content: right;
+	background-color: skyblue;
 `;
 
-const ChArticleItems = styled.div`
-  grid-column: 1;
-  grid-row: 2;
+const Nav = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	background-color: pink;
+	& > div:nth-child(odd) {
+		background-color: orange;
+	}
 `;
 
-const ChArticleItems2 = styled.div`
-  grid-column: 2;
-  grid-row: 2;
+const Article53 = styled.div`
+	display: grid;
+	grid-template-columns: 5fr 3fr;
+	background-color: green;
+	& > div:last-child {
+		background-color: red;
+	}
 `;
 
 export default () => (
-  <HomeLayout>
-    <ClubhouseHome>
-      <ChTittle className="clubhousehome-title">Clubhouse</ChTittle>
-      <Spacer></Spacer>
-      <ChTittleItems className="clubhousehome-title-items">
-        <a href="/">+new clubhouse</a>
-      </ChTittleItems>
-      <ChArticleItems className="clubhousehome-article-items">
-        <a href="/">1</a>
-      </ChArticleItems>
-      <ChArticleItems2>
-        <a href="/">2</a>
-      </ChArticleItems2>
-    </ClubhouseHome>
-    <Profile>Profile</Profile>
-    <Photo>Photo</Photo>
-    <MySchedule>My Schedule</MySchedule>
-    <MyRank>My Rank</MyRank>
-  </HomeLayout>
+	<>
+		<HomeLayout>
+			<Header>
+				<div>Clubhouse</div>
+				<ProfileLogo>
+					<div>Profile Logo</div>
+				</ProfileLogo>
+			</Header>
+			<Nav>
+				<div className="first">홈</div>
+				<div className="first">피드</div>
+				<div className="first">알람</div>
+				<div className="first">랭킹</div>
+			</Nav>
+			<Article53>
+				<div className="bgtest">clubhouse list</div>
+				<div>profile</div>
+			</Article53>
+			<div>사진</div>
+			<Article53>
+				<div>schedule</div>
+				<div>rank</div>
+			</Article53>
+		</HomeLayout>
+	</>
 );
---
+++ src/Routes/Feedhome.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import styled from "styled-components";
+import HomeLayout from "../Components/Layout/HomeLayout";
+import ClubhouseHome from "../Components/Layout/ClubhouseHome";
+import Profile from "../Components/Layout/Profile";
+import Photo from "../Components/Layout/Photo";
+import MySchedule from "../Components/Layout/MySchedule";
+import MyRank from "../Components/Layout/MyRank";
+
+const ChTittle = styled.div`
+  grid-column: 1;
+  grid-row: 1;
+`;
+
+const Spacer = styled.div`
+  grid-column: 2;
+  grid-row: 1;
+  flex: 1;
+`;
+
+const ChTittleItems = styled.div`
+  grid-column: 3;
+  grid-row: 1;
+`;
+
+const ChArticleItems = styled.div`
+  grid-column: 1;
+  grid-row: 2;
+`;
+
+const ChArticleItems2 = styled.div`
+  grid-column: 2;
+  grid-row: 2;
+`;
+
+export default () => (
+  <HomeLayout>
+    <ClubhouseHome>
+      <ChTittle className="clubhousehome-title">Clubhouse</ChTittle>
+      <Spacer></Spacer>
+      <ChTittleItems className="clubhousehome-title-items">
+        <a href="/">+new clubhouse</a>
+      </ChTittleItems>
+      <ChArticleItems className="clubhousehome-article-items">
+        <a href="/">1</a>
+      </ChArticleItems>
+      <ChArticleItems2>
+        <a href="/">2</a>
+      </ChArticleItems2>
+    </ClubhouseHome>
+    <Profile>Profile</Profile>
+    <Photo>Photo</Photo>
+    <MySchedule>My Schedule</MySchedule>
+    <MyRank>My Rank</MyRank>
+  </HomeLayout>
+);
